69d4b68cbc8d0134db7b21a4158a5c9df1ba3096
mouhcine <mouhcine.toumi98@gmail.com>
2019-12-22 01:53:55 +0100

    nodes keep their childs too

--- src/main/java/logic/Node.java
+++ src/main/java/logic/Node.java
@@ -5,15 +5,30 @@ import java.util.List;
 
 public class Node {
 
+
     String label;
-    boolean contaminated;
+    boolean contaminated, visited;
     List<Node> childs;
-
-
 
     public Node(String label){
         this.label = label;
         contaminated =false;
+        visited = false;
         childs = new ArrayList<Node>();
+    }
+
+    public boolean isChild(String label) {
+        for ( Node node: childs){
+            if ( node.label == label){
+                return  true;
+            }
+        }
+        return false;
+    }
+
+    public void addChild(Node node) {
+        if( ! childs.contains(node)) {
+            childs.add(node);
+        }
     }
 }
